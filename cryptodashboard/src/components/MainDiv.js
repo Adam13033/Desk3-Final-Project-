23a7fd0726fe87d537b99fd1bd2b4e7c1b635f98
AutumnMogg <sammarsh@gmail.com>
2022-04-28 15:19:05 +0100

    Broken Carousel

--- cryptodashboard/src/components/MainDiv.js
+++ cryptodashboard/src/components/MainDiv.js
@@ -1,24 +1,44 @@
 import React from "react";
+import Carousel from "./Carousel";
 import Navbar from "./Navbar";
+import Ethbtcchart from "./charts/dashboardCharts";
+import "../components/styles/MainDiv.css";
 
 const MainDiv = () => {
   return (
     <div className="LandingPage">
-      <div className="Navbar">
+      <div>
         <Navbar />
       </div>
-      <div className="TopImage"></div>
-      <div className="BlockChainTiles">
-        <div className="TopTiles">
-          <div className="Bitcoin">1</div>
-          <div className="Ethereum">2</div>
+      <div className="Content">
+        <div className="CarouselSect">
+        <Carousel />
         </div>
-        <div className="BottomTiles">
-          <div className="Monero">3</div>
-          <div className="XRP">4</div>
+        <div className="Charts">
+      
+          <Ethbtcchart />
         </div>
+        <div className="Table"> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
       </div>
     </div>
+
+    // <div className="LandingPage">
+    //   <div className="Navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="TopImage"></div>
+    //     {/* <Carousel /> */}
+    //   <div className="BlockChainTiles">
+    //     <div className="TopTiles">
+    //       <div className="Bitcoin">1</div>
+    //       <div className="Ethereum">2</div>
+    //     </div>
+    //     <div className="BottomTiles">
+    //       <div className="Monero">3</div>
+    //       <div className="XRP">4</div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
