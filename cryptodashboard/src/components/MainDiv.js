8fd9c0d468b76f17b40b5bbdefd75448b7aa6c5f
AutumnMogg <sammarsh@gmail.com>
2022-04-29 19:06:55 +0100

    Changed lots of CSS and developed the table component further

--- cryptodashboard/src/components/MainDiv.js
+++ cryptodashboard/src/components/MainDiv.js
@@ -3,29 +3,26 @@ import Ethbtcchart from "../components/charts/dashboardCharts";
 import Carousel from "./Carousel";
 import Navbar from "./Navbar";
 import { CoinsTable } from "./Table";
+import "./styles/MainDiv.css";
 
 const MainDiv = () => {
   return (
     <div className="LandingPage">
-     
-    <div>
-    <Navbar />
-      
-    </div>
-    <div className="Content">
-
-      <div className="CarouselSect">
-      <Carousel />
+      <div>
+        <Navbar />
       </div>
-      <div className="Charts">
-    
-        <Ethbtcchart />
-      </div>
-      <div className="Table">
-        <CoinsTable />
+      <div className="Content">
+        <div className="CarouselSect">
+          <Carousel />
+        </div>
+        <div className="Charts">
+          <Ethbtcchart />
+        </div>
+        <div className="Table">
+          <CoinsTable />
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
